bcb0e706d98d70ba8e5e17a3da1d6a3bb0236a51
”Alexey <alex.vesson.smith@gmail.com>
2021-05-20 23:05:43 +0300

    first commit

---
+++ src/components/ShopItemFunc.jsx
@@ -0,0 +1,18 @@
+export default function ShopItemFunc(props) {
+  const {item} = props;
+
+  return (
+    <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div class="description">{item.descriptionFull}</div>
+      <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+      <div class="divider"></div>
+      <div class="purchase-info">
+        <div class="price">{item.currency + item.price.toFixed(2)}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  )
+}
